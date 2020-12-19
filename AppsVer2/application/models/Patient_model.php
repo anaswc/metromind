07eeff73d371be1a2a74570fdcd86f23e960c708
Sreeraj T <sreeraj@webcastle.in>
2020-12-19 15:25:05 +0530

    patient model changes

--- AppsVer2/application/models/Patient_model.php
+++ AppsVer2/application/models/Patient_model.php
@@ -2814,7 +2814,7 @@ public function cancel_appointment_patient($appointmentId)
 
 		if(count($result_array) > 0){
 			
-			$this->push_notification_after_payment($result_array[0]["uniqueId"],$result_array[0]["fcmToken"]);
+			$this->push_notification_after_payment($result_array[0]["uniqueId"],$result_array[0]["fcmToken"],$notificationTitle,$doc_id);
 
 			
 		}else{
@@ -2824,7 +2824,7 @@ public function cancel_appointment_patient($appointmentId)
 		}
 	}
 
-	public function push_notification_after_payment($cs_uniqueid,$cs_fcmtoken){
+	public function push_notification_after_payment($cs_uniqueid,$cs_fcmtoken,$notificationTitle,$doc_id){
 
 		
 		
