e124f1257b75de685602444374491f54d98bd29d
webcastle support <support@webcastle.in>
2021-01-21 15:41:49 +0530

    database

--- AppsVer2/application/config/database.php
+++ AppsVer2/application/config/database.php
@@ -610,10 +610,10 @@ $db['default'] = array(
 
 
 	'username' => 'root',
-	// 'password' => '',
+	// 'password' => 'a',
 	// 'username' => 'metromindapp_app_full',
-	// 'password' => '23!45f!vWDDQdpj8',
-	'password' => 'h4QvwrbkE4sYFUzA',
+	'password' => '23!45f!vWDDQdpj8',
+	// 'password' => 'h4QvwrbkE4sYFUzA',
 	
 
 
