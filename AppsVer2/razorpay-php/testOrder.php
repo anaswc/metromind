9d1e0830b681b5595927ffa48fd5dd4415320840
webcastle support <support@webcastle.in>
2021-01-22 11:31:04 +0530

    db

--- AppsVer2/razorpay-php/testOrder.php
+++ AppsVer2/razorpay-php/testOrder.php
@@ -4,10 +4,12 @@ echo "order1->".$order;
 include("Razorpay.php");
 use Razorpay\Api\Api;
 echo "order2->".$order;
-// $api_key = 'rzp_live_abEAlYmmQUzUCg';
-$api_key = 'rzp_test_aNY9pateh9pWYw';
+$api_key = 'rzp_live_abEAlYmmQUzUCg';
+// $api_key = 'rzp_test_aNY9pateh9pWYw';
 
-$api_secret = 'Jnkou0WzPZ64R3eF3baAk2j8';
+// $api_secret = 'Jnkou0WzPZ64R3eF3baAk2j8';
+$api_secret = 'Z89KjX25P47SxYxWRZvp0JL2';//live
+
 // iHloBNTVN4s8CYJzeX8MzxfB
 // $api_secret = 'BvrldbyJ1smnv3wU6LEa3QTX';
 echo "order3->".$order;
