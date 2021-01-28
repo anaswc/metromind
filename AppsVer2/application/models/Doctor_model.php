dff8c66f3955f3f0724e8f4895601742faa49d5f
Sreeraj T <sreeraj@webcastle.in>
2021-01-28 11:41:56 +0530

    chages

--- AppsVer2/application/models/Doctor_model.php
+++ AppsVer2/application/models/Doctor_model.php
@@ -3084,8 +3084,10 @@ public function get_doctor_appointment_by_date($doctorId,$appointmentdate,$avail
 	$this->db->where('appointmentdate', $appointmentdate );
 	 $this->db->where('appointmentStartTime', $availableStartTime);
 		//$this->db->where('appointmentEndTime', $availableEndTime);
-	$this->db->where('status',0);
-	$this->db->or_where('status',1);
+	$this->db->where('status !=',2);
+	$this->db->or_where('status!=',3);
+	$this->db->or_where('status!=',4);
+	$this->db->or_where('status!=',5);
 		
 
 		
