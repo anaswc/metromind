bf798dcf53dfa6e88534f666df8223164472076f
Sreeraj T <sreeraj@webcastle.in>
2021-02-13 13:19:30 +0530

    changes

--- AppsVer2/razorpay-php/testOrder.php
+++ AppsVer2/razorpay-php/testOrder.php
@@ -4,8 +4,8 @@ echo "order1->".$order;
 include("Razorpay.php");
 use Razorpay\Api\Api;
 echo "order2->".$order;
-// $api_key = 'rzp_live_abEAlYmmQUzUCg';
-$api_key = 'rzp_test_qNzNL7FiDVFoQH';
+$api_key = 'rzp_live_abEAlYmmQUzUCg';
+// $api_key = 'rzp_test_qNzNL7FiDVFoQH';
 
 $api_secret = 'Jnkou0WzPZ64R3eF3baAk2j8';
 // $api_secret = 'Z89KjX25P47SxYxWRZvp0JL2';//live
